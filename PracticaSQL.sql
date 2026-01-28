f39349351c1e700a2768fa05b6cb2f41330ad4e5
Donald Reyes <donald900reyes@gmail.com>
2026-01-27 18:48:18 -0600

    Mas comandos practicos JOIN/AVG/Group BY

--- PracticaSQL.sql
+++ PracticaSQL.sql
@@ -21,7 +21,7 @@ SELECT ProductName,UnitPrice,UnitsinStock FROM Products WHERE UnitsInStock>0 AND
 --4 Busqueda de patrones (Like)
 SELECT CompanyName, ContactName FROM Customers WHERE CompanyName LIKE 'A%';
 
-SELECT * FROM Employees where Title LIKE 'Manager_%' OR Title LIKE '%_Manager'
+SELECT * FROM Employees where Title LIKE '%Manager%'
 
 --5 Ordenamiento
 
@@ -34,3 +34,35 @@ SELECT * FROM Orders ORDER BY OrderDate DESC
 SELECT COUNT(*) FROM Products
 
 SELECT COUNT(*)From Customers WHERE Country='USA' 
+
+--Fase 2 Conectar Tablas y agrupar datos
+
+--Unir tablas (INNER JOIN)
+
+--Listar productos junto con el nombre de la categoria
+SELECT Products.ProductName, Categories.CategoryName
+FROM Products
+INNER JOIN Categories ON Products.CategoryID= Categories.CategoryID;
+
+--Lista de pedidos que incluya orderID y fecha
+--con el nombre de la compañia del cliente
+
+SELECT o.OrderID , o.OrderDate, Customers.CompanyName
+FROM Orders o
+INNER JOIN Customers ON o.CustomerID= Customers.CustomerID;
+
+
+--Agregacion basica(SUM,AVG,MAX,MIN)
+--cual es el precio promedio de todos nuestros productos
+
+SELECT AVG(UnitPrice) as [Precio Promedio] FROM Products;
+
+--Suma total del transporte de todos los pedidos en la tabla Orders
+
+SELECT SUM(Freight) as [Suma total transporte] FROM Orders;
+
+--Agrupacion de datos(GROUP BY)
+
+--¿Cuantos productos nos provee cada Suplidor?
+SELECT SupplierID, COUNT(*) FROM Products
+GROUP BY SupplierID;
